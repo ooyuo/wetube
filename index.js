41b135b080785393feb0e072edf8842e466bdb6a
doyounjeong <doyounjeong0@gmail.com>
2019-11-23 16:42:42 +0900

    #2.4 Handling Routes with Express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hi');
+const express = require('express') // express라는 이름을 내 파일들안엣 찾고, 못찾으면 node_modules>express>index.js
+const app = express();
+
+const PORT = 4000;
+function handleListening() {
+    console.log(`listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
