4ee9f75e1b00214e90cbdaf1420dfc538786ae48
doyounjeong <doyounjeong0@gmail.com>
2019-11-24 22:47:38 +0900

    #2.5 ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require('express') // express라는 이름을 내 파일들안에서 찾고, 못찾으면 node_modules> express> index.js 순으로 찾는다.
-const app = express(); // server생성
+import express from "express" // express라는 이름을 내 파일들안에서 찾고, 못찾으면 node_modules> express> index.js 순으로 찾는다.
+const app = express() // server생성
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res) { // 응답
-    res.send('hello from home');
-}
 
-function handleProfile(req, res) {
-    res.send("you are on my profile");
-}
+const handleHome = (req, res) =>  res.send('hello from my ass');
+
+const handleProfile = (req, res) => res.send("you are on my profile");
+
 app.get("/", handleHome); // route 생성
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
