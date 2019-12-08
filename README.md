a84d4986c46b3b9d17de8d67bd94447f7ca14b42
doyounjeong <doyounjeong0@gmail.com>
2019-12-08 22:10:28 +0900

    #2.19 Join : Log in HTML

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # WeTube
+
+Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
+
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.send("Join", { pateTitle: "Home"});
-export const login = (req, res) => res.send("Login", { pateTitle: "Home"});
-export const logout = (req, res) => res.send("Logout", { pateTitle: "Home"});
-export const users = (req, res) => res.send("Users", { pateTitle: "Home"});
-export const userDetail = (req, res) => res.send("User Detail", { pateTitle: "Home"});
-export const editProfile = (req, res) => res.send("Edit Profile", { pateTitle: "Home"});
-export const changePassword = (req, res) => res.send("Change Password", { pateTitle: "Home"});
+export const join = (req, res) => res.render("Join", { pateTitle: "Home"});
+export const login = (req, res) => res.render("Login", { pateTitle: "Home"});
+export const logout = (req, res) => res.render("Logout", { pateTitle: "Home"});
+export const users = (req, res) => res.render("Users", { pateTitle: "Home"});
+export const userDetail = (req, res) => res.render("User Detail", { pateTitle: "Home"});
+export const editProfile = (req, res) => res.render("Edit Profile", { pateTitle: "Home"});
+export const changePassword = (req, res) => res.render("Change Password", { pateTitle: "Home"});
