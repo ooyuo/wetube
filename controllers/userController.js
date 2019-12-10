ec5532ce7cdba048a8009870876088c39fd2fcff
doyounjeong <doyounjeong0@gmail.com>
2019-12-11 00:13:43 +0900

    #2.20 Change Profile HTML

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,6 @@ export const join = (req, res) => res.render("Join", { pateTitle: "Home"});
 export const login = (req, res) => res.render("Login", { pateTitle: "Home"});
 export const logout = (req, res) => res.render("Logout", { pateTitle: "Home"});
 export const users = (req, res) => res.render("Users", { pateTitle: "Home"});
-export const userDetail = (req, res) => res.render("User Detail", { pateTitle: "Home"});
-export const editProfile = (req, res) => res.render("Edit Profile", { pateTitle: "Home"});
-export const changePassword = (req, res) => res.render("Change Password", { pateTitle: "Home"});
+export const userDetail = (req, res) => res.render("UserDetail", { pateTitle: "Home"});
+export const editProfile = (req, res) => res.render("EditProfile", { pateTitle: "Home"});
+export const changePassword = (req, res) => res.render("ChangePassword", { pateTitle: "Home"}); // ""안에있는 라우터명은 routes파일의 객체명과 일치해야함
