79c5bfd53286c090d4443eb66a6c5f56e9cc144c
doyounjeong <doyounjeong0@gmail.com>
2019-12-26 23:08:14 +0900

    #3.6 Uploading and Creating a Video

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,7 @@
+import multer from "multer";
 import routes from "./routes";
+
+const multerVideo = multer({ dest: "videos/"});
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube"; // locals에 있는 건 템플릿에 변수명처럼 존재한다.
@@ -9,3 +12,5 @@ export const localsMiddleware = (req, res, next) => {
     };
     next();
 };
+
+export const uploadVideo = multerVideo.single("videoFile"); // videoFile: upload.pug> form의 input name
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
 import { getUpload, postUpload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload); // uploadVideo middleware(video file> url로변환)
 
 videoRouter.get(routes.videoDetail(), videoDetail);
 
