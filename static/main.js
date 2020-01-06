a4910376295a12dcfb89ee19afef6a3e70e0b017
doyounjeong <doyounjeong0@gmail.com>
2020-01-06 21:53:15 +0900

    #5.0 SCSS and Making the Header

--- static/main.js
+++ static/main.js
@@ -105,7 +105,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scs
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./assets/scss/styles.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/extract-text-webpack-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Invalid CSS after \\\"...nt-weight: 600;\\\": expected \\\"}\\\", was \\\"&:::placeholder {\\\"\\n        on line 41 of assets/scss/partials/header.scss\\n        from line 4 of C:\\\\documents\\\\wetube\\\\assets\\\\scss\\\\styles.scss\\n>>                     font-weight: 600;\\r\\n   -------------------------------------^\\n\\n    at C:\\\\documents\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:316:20\\n    at C:\\\\documents\\\\wetube\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at C:\\\\documents\\\\wetube\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:233:18\\n    at context.callback (C:\\\\documents\\\\wetube\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:111:13)\\n    at Object.callback (C:\\\\documents\\\\wetube\\\\node_modules\\\\sass-loader\\\\dist\\\\index.js:73:7)\\n    at Object.done [as callback] (C:\\\\documents\\\\wetube\\\\node_modules\\\\neo-async\\\\async.js:8067:18)\\n    at options.error (C:\\\\documents\\\\wetube\\\\node_modules\\\\node-sass\\\\lib\\\\index.js:294:32)\");\n\n//# sourceURL=webpack:///./assets/scss/styles.scss?");
 
 /***/ }),
 
