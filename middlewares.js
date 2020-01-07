e5e8bc2c739e2e0629d16c7bbd11748b86ce3391
doyounjeong <doyounjeong0@gmail.com>
2020-01-07 22:33:45 +0900

    #5.2 Everything else

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube"; // locals에 있는 건 템플릿에 변수명처럼 존재한다.
     res.locals.routes = routes;
     res.locals.user = {
-        isAuthenticated: false,
+        isAuthenticated: true,
         id: 1
     };
     next();
