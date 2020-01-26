24f87ca8d3e4f5e1cf9e4a160a60f1cc3b1d9d19
doyounjeong <doyounjeong0@gmail.com>
2020-01-26 19:06:10 +0900

    #6.11 Facebook Login Part Two

--- passport.js
+++ passport.js
@@ -28,7 +28,9 @@ passport.use(
   new FacebookStrategy({
     clientID: process.env.FB_ID,
     clientSecret: process.env.FB_SECRET,
-    callbackURL: `http://localhost:4000${routes.facebookCallback}`
+    callbackURL: `https://baba1023.ngrok.io${routes.facebookCallback}`,
+    profileFields: ["id", "displayName", "photos", "email"],
+    scope: ["public_profile", "email"],
   },
   facebookLoginCallback
   )
