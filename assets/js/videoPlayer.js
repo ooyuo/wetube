51d2abdd9bf8dbe921033097104d4dd58edf349d
doyounjeong <doyounjeong0@gmail.com>
2020-02-05 21:44:39 +0900

    JS Casino

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -30,13 +30,50 @@ function exitFullScreen() {
     fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
     fullScrnBtn.addEventListener("click", goFullScreen);
     document.exitFullscreen();
+    if(document.exitFullScreen) {
+        document.exitFullScreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullScreen) {
+        document.webkitExitFullScreen();
+    } else if (document.msExitFullScreen) {
+        document.msExitFullScreen();
+
+    }
 }
 
 function goFullScreen() {
+    if (videoContainer.requestFullscreen) {
+        videoContainer.requestFullscreen();
+    } else if (videoContainer.mozCancelFullScreen) {
+        videoContainer.mozCancelFullScreen();
+    } else if (videoContainer.webkitRequestFullScreen) {
+        videoContainer.webkitRequestFullScreen();
+    } else if (videoContainer.msRequestFullScreen) {
+        videoContainer.msRequestFullScreen();
+    }
     videoContainer.requestFullscreen();
     fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
     fullScrnBtn.removeEventListener("click", goFullScreen);
     fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
+const formDate = seconds => {
+    const secondsNumber = parseInt(seconds, 10);
+    let hours = Math.floor(secondsNumber / 3600);
+    let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+    let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+    
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+        totalSeconds = `0${seconds}`;
+    }
+    return `0${hours}:0${minutes}:${totalSeconds}`;
 }
 
 function init() {
